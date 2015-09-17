5662947236cc6e84a5a386d8e7c2ce6404c2bee7
oliver <olivercs@gmail.com>
2015-09-17 16:47:05 -0400

    Open a pass locally and check if a pass is in the Pass Library.

--- src/ios/CDVPassbook.h
+++ src/ios/CDVPassbook.h
@@ -1,8 +1,14 @@
 #import <Cordova/CDVPlugin.h>
+#import <PassKit/PassKit.h>
+
 
 @interface CDVPassbook : CDVPlugin
 
-- (void)available:(CDVInvokedUrlCommand*)command;
-- (void)downloadPass:(CDVInvokedUrlCommand*)command;
+@property(nonatomic, assign, nullable) id < PKAddPassesViewControllerDelegate > delegate;
 
+- (void)available:(CDVInvokedUrlCommand*)command;
+- (void)isPassInLibrary:(CDVInvokedUrlCommand*)command;
+- (void)openPass:(CDVInvokedUrlCommand*)command;
+- (void)downloadPass:(CDVInvokedUrlCommand*)command;
+- (void)addPassesViewControllerDidFinish:(PKAddPassesViewController *)controller;
 @end
--- www/passbook.js
+++ www/passbook.js
@@ -5,7 +5,16 @@ var exec = require('cordova/exec');
 passbook.available = function (resultCallback) {
     return exec(resultCallback, null, "Passbook", "available", []);
 };
-               
+
+passbook.isPassInLibrary = function (path,resultCallback) {
+    return exec(resultCallback, null, "Passbook", "isPassInLibrary", [path]);
+};
+
+passbook.openPass = function (path, successCallback, errorCallback) {
+    return exec(successCallback, errorCallback, "Passbook", "openPass", [path]);
+};
+
 passbook.downloadPass = function (url, successCallback, errorCallback) {
     return exec(successCallback, errorCallback, "Passbook", "downloadPass", [url]);
 };
+
